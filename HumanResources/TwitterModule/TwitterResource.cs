e7a68fbdc373a43d769d26794d357a1ca19c9026
Tim Fielding <gnidleif@gmail.com>
2019-05-22 15:51:20 +0200

    twitter again

--- HumanResources/TwitterModule/TwitterResource.cs
+++ HumanResources/TwitterModule/TwitterResource.cs
@@ -1,8 +1,6 @@
-﻿using Discord;
-using HumanResources.Utilities;
-using Newtonsoft.Json;
+﻿using HumanResources.Utilities;
+using LinqToTwitter;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,7 +12,8 @@ namespace HumanResources.TwitterModule
     private static readonly Lazy<TwitterResource> lazy = new Lazy<TwitterResource>(() => new TwitterResource());
     private const string Name = "twitter.json";
     private readonly string Path = $"{Global.ResourceFolder}/{Name}";
-    private TwitterConfig Config { get; set; }
+    private TwitterConfig Cfg { get; set; } = new TwitterConfig();
+    private TwitterContext Ctx { get; set; }
 
     public static TwitterResource Instance { get { return lazy.Value; } }
 
@@ -24,25 +23,12 @@ namespace HumanResources.TwitterModule
 
     public bool Close()
     {
-      var toDelete = this.Config.Follows.Keys.Where(x => !this.Config.Follows[x].Any()).ToList();
-      toDelete.ForEach(x => this.Config.Follows.Remove(x));
-
-      return this.Save();
+      throw new NotImplementedException();
     }
 
-    public bool Contains(ulong gid)
+    public bool Contains(ulong gid, ulong uid)
     {
-      return this.Config.Follows.ContainsKey(gid);
-    }
-
-    public bool Contains(ulong gid, ulong cid)
-    {
-      return this.Contains(gid) && this.Config.Follows[gid].ContainsKey(cid);
-    }
-
-    public bool Contains(ulong gid, ulong cid, ulong uid)
-    {
-      return this.Contains(gid, cid) && this.Config.Follows[gid][cid].Users.Contains(uid);
+      throw new NotImplementedException();
     }
 
     public async Task Initialize()
@@ -52,89 +38,53 @@ namespace HumanResources.TwitterModule
         Directory.CreateDirectory(Global.ResourceFolder);
       }
       var temp = new TwitterConfig();
-      if (File.Exists(Path) ? JsonUtil.TryRead(Path, out temp) : JsonUtil.TryWrite(Path, temp))
+      if (File.Exists(this.Path) ? JsonUtil.TryRead(this.Path, out temp) : JsonUtil.TryWrite(this.Path, temp))
       {
-        this.Config = new TwitterConfig();
-        foreach (var gid in temp.Follows.Keys)
-        {
-          foreach (var cid in temp.Follows[gid].Keys)
-          {
-            if (this.Push(gid, cid))
-            {
-              this.Config.Follows[gid][cid].Users = temp.Follows[gid][cid].Users;
-            }
-          }
-        }
+        this.Cfg = temp;
+        await this.Authenticate();
       }
-      await Task.CompletedTask;
+    }
+
+    private async Task Authenticate()
+    {
+      var auth = new SingleUserAuthorizer
+      {
+        CredentialStore = new SingleUserInMemoryCredentialStore
+        {
+          ConsumerKey = this.Cfg.ConsumerKey,
+          ConsumerSecret = this.Cfg.ConsumerSecret,
+          AccessToken = this.Cfg.AccessToken,
+          AccessTokenSecret = this.Cfg.AccessTokenSecret,
+        },
+      };
+
+      try
+      {
+        await auth.AuthorizeAsync();
+        this.Ctx = new TwitterContext(auth);
+      }
+      catch (Exception e)
+      {
+        LogUtil.Write("TwitterResource:Authenticate", e.Message);
+      }
+    }
+
+    public bool Pop(ulong gid, ulong uid)
+    {
+      throw new NotImplementedException();
     }
 
     public bool Pop(ulong gid)
     {
-      if (this.Contains(gid))
-      {
-        return this.Config.Follows.Remove(gid);
-      }
-      return false;
+      throw new NotImplementedException();
     }
 
-    public bool Pop(ulong gid, ulong cid)
+    public bool Push(ulong gid, ulong uid)
     {
-      if (this.Contains(gid, cid))
-      {
-        return this.Config.Follows[gid].Remove(cid);
-      }
-      return false;
+      throw new NotImplementedException();
     }
 
-    public bool Pop(ulong gid, ulong cid, ulong uid)
-    {
-      if (this.Contains(gid, cid, uid))
-      {
-        return this.Config.Follows[gid][cid].Users.Remove(uid);
-      }
-      return false;
-    }
-
-    public bool Push(ulong gid, ulong cid)
-    {
-      if (!this.Config.Follows.ContainsKey(gid))
-      {
-        this.Config.Follows.Add(gid, new Dictionary<ulong, ChannelUsers>());
-      }
-      if (!this.Config.Follows[gid].ContainsKey(cid))
-      {
-        var g = Global.Client.GetGuild(gid);
-        if (g == null)
-        {
-          _ = this.Pop(gid);
-          return false;
-        }
-        var c = g.GetChannel(cid);
-        if (c == null)
-        {
-          _ = this.Pop(gid, cid);
-          return false;
-        }
-        this.Config.Follows[gid].Add(cid, new ChannelUsers
-        {
-          Channel = c as IMessageChannel,
-        });
-        return true;
-      }
-      return false;
-    }
-
-    public bool Push(ulong gid, ulong cid, ulong uid)
-    {
-      if (!this.Contains(gid, cid))
-      {
-        _ = this.Push(gid, cid);
-      }
-      return this.Config.Follows[gid][cid].Users.Add(uid);
-    }
-
-    public bool Save() => JsonUtil.TryWrite(this.Path, this.Config);
+    public bool Save() => JsonUtil.TryWrite(this.Path, this.Cfg);
   }
 
   public class TwitterConfig
@@ -143,13 +93,5 @@ namespace HumanResources.TwitterModule
     public string ConsumerSecret { get; set; } = string.Empty;
     public string AccessToken { get; set; } = string.Empty;
     public string AccessTokenSecret { get; set; } = string.Empty;
-    public Dictionary<ulong, Dictionary<ulong, ChannelUsers>> Follows { get; set; } = new Dictionary<ulong, Dictionary<ulong, ChannelUsers>>();
-  }
-
-  public class ChannelUsers
-  {
-    public HashSet<ulong> Users { get; set; } = new HashSet<ulong>();
-    [JsonIgnore]
-    public IMessageChannel Channel { get; set; }
   }
 }
