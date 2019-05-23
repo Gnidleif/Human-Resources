328eddbea378b8e7c3f4eee0539c131d53be609a
Tim Fielding <gnidleif@gmail.com>
2019-05-23 10:48:32 +0200

    twitter integration on its way

---
+++ HumanResources/TwitterModule/Twitter.cs
@@ -0,0 +1,62 @@
+﻿using Discord;
+using Discord.Commands;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HumanResources.TwitterModule
+{
+  [Group("twitter")]
+  public class Twitter : ModuleBase<SocketCommandContext>
+  {
+    [Command, Summary("Retrieves a Twitter user specified by handle/id")]
+    public async Task GetUser([Remainder] string identifier)
+    {
+      var user = await TwitterResource.Instance.GetUserAsync(identifier);
+      if (user != null)
+      {
+        var embed = new EmbedBuilder();
+        embed.WithAuthor($"{user.Name} (@{user.ScreenNameResponse})", user.ProfileImageUrl, $"https://twitter.com/{user.ScreenNameResponse}");
+        var rgb = uint.Parse(user.ProfileLinkColor.Replace("#", ""), System.Globalization.NumberStyles.HexNumber);
+        embed.WithDescription(user.Description);
+        embed.WithColor(new Color(rgb));
+        embed.WithFooter("ID: " + user.UserIDResponse, "https://images-ext-1.discordapp.net/external/bXJWV2Y_F3XSra_kEqIYXAAsI3m1meckfLhYuWzxIfI/https/abs.twimg.com/icons/apple-touch-icon-192x192.png");
+
+        await ReplyAsync("", false, embed.Build());
+      }
+    }
+
+    [Group("stalk")]
+    [RequireUserPermission(GuildPermission.Administrator)]
+    public class Stalk : ModuleBase<SocketCommandContext>
+    {
+      [Command, Summary("Start outputting any tweets a specified user does in the given channel")]
+      public async Task StalkUser(IMessageChannel ch, [Remainder] string identifier)
+      {
+        var user = await TwitterResource.Instance.GetUserAsync(identifier);
+        if (user != null)
+        {
+          var id = ulong.Parse(user.UserIDResponse);
+          if (TwitterResource.Instance.Push(id, ch.Id))
+          {
+            await ReplyAsync($":white_check_mark: Started stalking {user.Name} (@{user.ScreenNameResponse})");
+          }
+        }
+      }
+
+      [Command("list"), Summary("Return list of stalked users")]
+      public async Task StalkList()
+      {
+        var embed = new EmbedBuilder();
+        foreach(var c in Context.Guild.Channels)
+        {
+          var l = TwitterResource.Instance.GetUsersByChannelId(c.Id);
+          if (l.Any())
+          {
+            embed.AddField(c.Name, string.Join(", ", l.Select(x => x.ScreenNameResponse)));
+          }
+        }
+        await ReplyAsync("", false, embed.Build());
+      }
+    }
+  }
+}
--- HumanResources/TwitterModule/TwitterResource.cs
+++ HumanResources/TwitterModule/TwitterResource.cs
@@ -1,6 +1,7 @@
 ﻿using HumanResources.Utilities;
 using LinqToTwitter;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,7 +13,7 @@ namespace HumanResources.TwitterModule
     private static readonly Lazy<TwitterResource> lazy = new Lazy<TwitterResource>(() => new TwitterResource());
     private const string Name = "twitter.json";
     private readonly string Path = $"{Global.ResourceFolder}/{Name}";
-    private TwitterConfig Cfg { get; set; } = new TwitterConfig();
+    private TwitterInfo Info { get; set; } = new TwitterInfo();
     private TwitterContext Ctx { get; set; }
 
     public static TwitterResource Instance { get { return lazy.Value; } }
@@ -23,13 +24,13 @@ namespace HumanResources.TwitterModule
 
     public bool Close()
     {
-      throw new NotImplementedException();
+      var toDelete = this.Info.UserChannels.Keys.Where(x => !this.Info.UserChannels[x].Any()).ToList();
+      toDelete.ForEach(x => this.Pop(x));
+
+      return this.Save();
     }
 
-    public bool Contains(ulong gid, ulong uid)
-    {
-      throw new NotImplementedException();
-    }
+    public bool Contains(ulong uid, ulong cid) => this.Info.UserChannels.ContainsKey(uid) && this.Info.UserChannels[uid].Contains(cid);
 
     public async Task Initialize()
     {
@@ -37,10 +38,10 @@ namespace HumanResources.TwitterModule
       {
         Directory.CreateDirectory(Global.ResourceFolder);
       }
-      var temp = new TwitterConfig();
+      var temp = new TwitterInfo();
       if (File.Exists(this.Path) ? JsonUtil.TryRead(this.Path, out temp) : JsonUtil.TryWrite(this.Path, temp))
       {
-        this.Cfg = temp;
+        this.Info = temp;
         await this.Authenticate();
       }
     }
@@ -51,10 +52,10 @@ namespace HumanResources.TwitterModule
       {
         CredentialStore = new SingleUserInMemoryCredentialStore
         {
-          ConsumerKey = this.Cfg.ConsumerKey,
-          ConsumerSecret = this.Cfg.ConsumerSecret,
-          AccessToken = this.Cfg.AccessToken,
-          AccessTokenSecret = this.Cfg.AccessTokenSecret,
+          ConsumerKey = this.Info.ConsumerKey,
+          ConsumerSecret = this.Info.ConsumerSecret,
+          AccessToken = this.Info.AccessToken,
+          AccessTokenSecret = this.Info.AccessTokenSecret,
         },
       };
 
@@ -69,29 +70,63 @@ namespace HumanResources.TwitterModule
       }
     }
 
-    public bool Pop(ulong gid, ulong uid)
+    public bool Pop(ulong uid, ulong cid)
     {
-      throw new NotImplementedException();
+      if (this.Contains(uid, cid))
+      {
+        return this.Info.UserChannels[uid].Remove(cid);
+      }
+      return false;
     }
 
-    public bool Pop(ulong gid)
+    public bool Pop(ulong uid) => this.Info.UserChannels.Remove(uid);
+
+    public bool Push(ulong uid, ulong cid)
     {
-      throw new NotImplementedException();
+      if (!this.Info.UserChannels.ContainsKey(uid))
+      {
+        this.Info.UserChannels.Add(uid, new HashSet<ulong>());
+      }
+      if (!this.Contains(uid, cid))
+      {
+        this.Info.UserChannels[uid].Add(cid);
+        return true;
+      }
+      return false;
     }
 
-    public bool Push(ulong gid, ulong uid)
+    public bool Save() => JsonUtil.TryWrite(this.Path, this.Info);
+
+    public async Task<User> GetUserAsync(string identifier)
     {
-      throw new NotImplementedException();
+      var query = this.Ctx.User.Where(t => t.Type == UserType.Show);
+      ulong id = 0;
+      if (ulong.TryParse(identifier, out id))
+      {
+        query = query.Where(t => t.UserID == id || t.Name == identifier || t.ScreenName == identifier);
+      }
+      else
+      {
+        query = query.Where(t => t.Name == identifier || t.ScreenName == identifier);
+      }
+      return await query.SingleOrDefaultAsync();
     }
 
-    public bool Save() => JsonUtil.TryWrite(this.Path, this.Cfg);
+    public List<User> GetUsersByChannelId(ulong cid)
+    {
+      var ids = this.Info.UserChannels.Where(x => x.Value.Contains(cid)).Select(x => x.Key).ToList();
+      var res = new List<User>();
+      ids.ForEach(async x => res.Add(await this.GetUserAsync(x.ToString())));
+      return res;
+    }
   }
 
-  public class TwitterConfig
+  public class TwitterInfo
   {
     public string ConsumerKey { get; set; } = string.Empty;
     public string ConsumerSecret { get; set; } = string.Empty;
     public string AccessToken { get; set; } = string.Empty;
     public string AccessTokenSecret { get; set; } = string.Empty;
+    public Dictionary<ulong, HashSet<ulong>> UserChannels { get; set; } = new Dictionary<ulong, HashSet<ulong>>();
   }
 }
